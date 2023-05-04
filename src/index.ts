013892630264d8e9dd30018cab2295541e741f12
tamo-tmry <mollianium@gmail.com>
2023-05-04 22:52:38 +0900

    feat: 🎸 おうむ返しサンプルの追加

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,42 @@
 
 // TODO: LINE BOTへレスポンスを返す
 
-// MEMO: Sample
-exports.handler = async (event: any, context: any) => {
-  console.log('boot test', event, context)
+// MEMO: おうむ返しサンプル
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Success' }),
-  }
+const line = require('@line/bot-sdk')
+const channelSecret = process.env.LINE_CHANNEL_SECRET
+const channelAccessToken = process.env.LINE_CHANNEL_ACCESS_TOKEN
+const apiUrl = 'https://api.line.me/v2/bot/message/reply'
+
+function validateSignature(body: any, signature: any) {
+  const crypto = require('node:crypto')
+  const hash = crypto
+    .createHmac('sha256', channelSecret)
+    .update(body)
+    .digest('base64')
+  return hash === signature
+}
+
+exports.handler = async (event: any) => {
+  const body = JSON.parse(event.body)
+  const signature = event.headers['x-line-signature']
+
+  if (!validateSignature(event.body, signature)) return
+
+  const client = new line.Client({
+    channelAccessToken,
+  })
+
+  const events = body.events
+  events.forEach(async (event: any) => {
+    if (event.type === 'message' && event.message.type === 'text') {
+      const replyToken = event.replyToken
+      const userMessage = {
+        type: 'text',
+        text: event.message.text,
+      }
+
+      client.replyMessage(replyToken, userMessage)
+    }
+  })
 }
