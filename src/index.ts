b24c7f3733b7e0040dd2ea28abb71131c2bdb8ea
tamo-tmry <mollianium@gmail.com>
2023-05-07 19:09:27 +0900

    fix: 🐛 おうむ返し実装の修正

--- src/index.ts
+++ src/index.ts
@@ -9,14 +9,19 @@
 // TODO: LINE BOTへレスポンスを返す
 
 // MEMO: おうむ返しサンプル
+import {
+  WebhookEvent,
+  WebhookRequestBody,
+  Message,
+  Client,
+} from '@line/bot-sdk'
+import { APIGatewayEvent } from 'aws-lambda'
+import crypto from 'crypto'
 
-const line = require('@line/bot-sdk')
-const channelSecret = process.env.LINE_CHANNEL_SECRET
-const channelAccessToken = process.env.LINE_CHANNEL_ACCESS_TOKEN
-const apiUrl = 'https://api.line.me/v2/bot/message/reply'
+const channelSecret = process.env.LINE_CHANNEL_SECRET!
+const channelAccessToken = process.env.LINE_CHANNEL_ACCESS_TOKEN!
 
-function validateSignature(body: any, signature: any) {
-  const crypto = require('node:crypto')
+function validateSignature(body: string, signature: string) {
   const hash = crypto
     .createHmac('sha256', channelSecret)
     .update(body)
@@ -24,26 +29,45 @@ function validateSignature(body: any, signature: any) {
   return hash === signature
 }
 
-exports.handler = async (event: any) => {
-  const body = JSON.parse(event.body)
+exports.handler = async (event: APIGatewayEvent) => {
+  const body: WebhookRequestBody = JSON.parse(event.body!)
   const signature = event.headers['x-line-signature']
 
-  if (!validateSignature(event.body, signature)) return
+  if (!validateSignature(event.body!, signature!)) {
+    return {
+      statusCode: 400,
+      body: 'FAILED',
+    }
+  }
 
-  const client = new line.Client({
+  const client = new Client({
     channelAccessToken,
   })
 
   const events = body.events
-  events.forEach(async (event: any) => {
-    if (event.type === 'message' && event.message.type === 'text') {
-      const replyToken = event.replyToken
-      const userMessage = {
-        type: 'text',
-        text: event.message.text,
-      }
 
-      client.replyMessage(replyToken, userMessage)
+  try {
+    await Promise.all(
+      events.map(async (event: WebhookEvent) => {
+        if (event.type === 'message' && event.message.type === 'text') {
+          const replyToken = event.replyToken
+          const userMessage: Message = {
+            type: 'text',
+            text: event.message.text,
+          }
+
+          return client.replyMessage(replyToken, userMessage)
+        }
+      }),
+    )
+    return {
+      statusCode: 200,
+      body: JSON.stringify('SUCCESS'),
     }
-  })
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: 'FAILED',
+    }
+  }
 }
