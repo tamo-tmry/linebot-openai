d5ee30e185b1a0a92595aaa0b84fb6e344b65e63
tamo-tmry <mollianium@gmail.com>
2023-05-07 20:22:08 +0900

    feat: 🎸 OpenAI APIのリクエスト処理追加

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,20 @@
-// TODO: LINE BOTからのリクエスト受け取り
-
-// TODO: DynamoDBから一番新しい履歴を取得
-
-// TODO: OpenAI APIへのリクエスト
-
-// TODO: OpenAI APIからのレスポンスをDynamoDBへ保存
-
-// TODO: LINE BOTへレスポンスを返す
-
-// MEMO: おうむ返しサンプル
 import {
   WebhookEvent,
   WebhookRequestBody,
   Message,
   Client,
 } from '@line/bot-sdk'
+import {
+  ChatCompletionRequestMessageRoleEnum,
+  Configuration,
+  OpenAIApi,
+} from 'openai'
 import { APIGatewayEvent } from 'aws-lambda'
 import crypto from 'crypto'
 
 const channelSecret = process.env.LINE_CHANNEL_SECRET!
 const channelAccessToken = process.env.LINE_CHANNEL_ACCESS_TOKEN!
+const openaiApiKey = process.env.OPENAI_API_KEY!
 
 function validateSignature(body: string, signature: string) {
   const hash = crypto
@@ -44,6 +39,11 @@ exports.handler = async (event: APIGatewayEvent) => {
     channelAccessToken,
   })
 
+  const configuration = new Configuration({
+    apiKey: openaiApiKey,
+  })
+  const openai = new OpenAIApi(configuration)
+
   const events = body.events
 
   try {
@@ -51,11 +51,36 @@ exports.handler = async (event: APIGatewayEvent) => {
       events.map(async (event: WebhookEvent) => {
         if (event.type === 'message' && event.message.type === 'text') {
           const replyToken = event.replyToken
-          const userMessage: Message = {
-            type: 'text',
-            text: event.message.text,
+          const message = event.message.text
+          const commonMessage = {
+            role: ChatCompletionRequestMessageRoleEnum.System,
+            content:
+              'タメ口だけど優しく、絵文字もたくさん使いながら喋ってください。',
           }
 
+          // TODO: DynamoDBから一番新しい履歴を取得
+
+          const response = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: [
+              commonMessage,
+              {
+                role: ChatCompletionRequestMessageRoleEnum.User,
+                content: message,
+              },
+            ],
+          })
+
+          const answer = response.data.choices[0].message?.content
+
+          // TODO: OpenAI APIからのレスポンスをDynamoDBへ保存
+          if (answer) {
+          }
+
+          const userMessage: Message = {
+            type: 'text',
+            text: answer || '失敗しちゃった。もう一回試してね。',
+          }
           return client.replyMessage(replyToken, userMessage)
         }
       }),
