752fc1c7138bf8180d3898905e0181edf6905914
tamo-tmry <mollianium@gmail.com>
2023-06-02 23:48:19 +0900

    feat: 🎸 promptの内容を調整

--- src/index.ts
+++ src/index.ts
@@ -127,7 +127,7 @@ exports.handler = async (event: APIGatewayEvent) => {
 
           if (Boolean(imageGenerationKeyword)) {
             const keywordRemovalPattern = new RegExp(
-              `の?${imageGenerationKeyword}`,
+              `の?${imageGenerationKeyword}.*`,
             )
             const promptMessage = message.replace(keywordRemovalPattern, '')
             console.log('DEBUG promptMessage: ', promptMessage)
